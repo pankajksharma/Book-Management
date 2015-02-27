48e56723f5e69dd58148c0cb47db249863e0399b
zwpwjwtz <zwpwjwtz@126.com>
2015-02-27 18:50:42 +0800

    Seperate MySQL configuration from incl_user.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('SQL_HOST','localhost');
+define('SQL_USER','test');
+define('SQL_PASSWORD','password');
+define('SQL_DB','book');
+
+?>
--- includes/incl_user.php
+++ includes/incl_user.php
@@ -1,4 +1,5 @@
 <?php
-  $con=mysqli_connect('localhost','test','password') or die("Cannot connect to DB ");
-    mysqli_select_db($con,"book")or die("cannot select DB");
+  require_once('./config.php');
+  $con=mysqli_connect(SQL_HOST,SQL_USER,SQL_PASSWORD) or die("Cannot connect to DB ");
+    mysqli_select_db($con,SQL_DB)or die("cannot select DB");
 ?>
