77dda297a2a931caeb7a646e3348e5dfd0fdf438
zwpwjwtz <zwpwjwtz@126.com>
2016-03-18 22:59:04 +0800

    Add script for importing sql; integrate comments into sql file

--- install/book_books.sql
+++ install/book_books.sql
@@ -5,11 +5,11 @@ CREATE TABLE book_books(
 			bookname varchar(30) not null,
 			authorsname varchar(30),
 			edition int default 0,
-			cond varchar(30),
-			price int not null,
-			type_id int not null,
+			cond varchar(30),            /* Condition */
+			price int not null,         /* Expected Price */
+			type_id int not null,       /* Field */
 			subject varchar(30),
-			rec_year varchar(30),
-			ind_pic varchar(50),
+			rec_year varchar(30),        /* Recommended Year */
+			ind_pic varchar(50),         /* Picture */
 			primary key(book_id)
 			) default charset=utf8;
--- install/book_users.sql
+++ install/book_users.sql
@@ -1,19 +1,19 @@
 CREATE TABLE book_users(
-			sno smallint AUTO_INCREMENT,
+			sno smallint AUTO_INCREMENT, /* User ID */
 			user_type int default 0,
 			username VARCHAR(15),
 			firstname VARCHAR(25),
 			lastname VARCHAR(25),
-			pwd VARCHAR(20),
+			pwd VARCHAR(20),             /* Password */
 			email_id VARCHAR(25),
 			phone VARCHAR(15),
 			college VARCHAR(30),
 			adr_line1 VARCHAR(20) ,
-			adr_line2 VARCHAR(20),
+			adr_line2 VARCHAR(20),       /* Address(optional) */
 			adr_city VARCHAR(30),
 			adr_state VARCHAR(30),
 			adr_coun VARCHAR(30),
-			nobs int,
+			nobs int,                    /* Number of Books to Sold*/
 			joined_on datetime default CURRENT_TIMESTAMP ,
 			logged_in int default 0,
 			primary key(sno)
